bd4f92f02c3b0c901308c3132744ced09f4c8de8
Laurinwinfo <lau-ps4@gmx.de>
2025-05-06 16:09:04 +0200

    javascript für aufgabe 1 in validierung.js nicht in login da es übergreifend genutzt werden muss

---
+++ assets/javascript/Validierung.js
@@ -0,0 +1,38 @@
+
+/* Skript für Login */
+
+let benutzernameinput =document.getElementById("usernamelogin").addEventListener("input",);
+let passwortinput = document.getElementById("userpasswordlogin");
+let änderungen = addEventListener;
+
+if (benutzernameinput === 'lol' && passwortinput === '123'){
+    window.alert("lol");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* Skript für Registration */
+
+
+/* Script für pop up fenster (direction button von registration zurück zu startseite zu kommen)*/
+function confirmAction(message) {
+    if (confirm(message)) {
+        window.location = "../index.html";
+    }
+}
+
+
+
+
