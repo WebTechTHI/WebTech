e9e6ca5d454206c4954ea86c8d068dd097cdfb6e
micha <pietschi2003@gmail.com>
2025-05-27 23:58:45 +0200

    html in php

--- testIndex.php
+++ testIndex.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>MLR - Gaming PCs, Laptops & High-End Computer</title>
 
-    <link rel="stylesheet" href="/assets/css/tests/testIndex.css">
+    <link rel="stylesheet" href="/webtech/WebTech/assets/css/tests/testIndex.css">
 
 </head>
 
