03b10146fcd5320dda00e7d612428eba2b6dd05c
ris2830 <ris2830@thi.de>
2025-05-28 02:24:59 +0200

    test

---
+++ productLists/category.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="de">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MLR - Gaming PCs, Laptops & High-End Computer</title>
+
+    <link rel="stylesheet" href="/assets/css/index.css">
+    <link rel="stylesheet" href="/assets/css/mystyle.css">
+
+</head>
+
+<body>
+
+    <?php
+$path = __DIR__ . '/components/header.html';
+if (file_exists($path)) {
+    echo "Datei gefunden: $path";
+} else {
+    echo "Datei NICHT gefunden: $path";
+}
+?>
+
+
+
+    
+    
+</html>
