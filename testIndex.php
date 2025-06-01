58732b314a51814a00fd3e326b67d6d5465b5f34
micha <pietschi2003@gmail.com>
2025-06-01 18:59:05 +0200

    a

--- testIndex.php
+++ testIndex.php
@@ -9,7 +9,7 @@
     <link rel="stylesheet" href="/assets/css/index.css">
     <link rel="stylesheet" href="/assets/css/mystyle.css">
 
-    <script src="/assets/javascript/toggleTheme.js">
+    <script src="/assets/javascript/toggleTheme.js"></script>
 
 </head>
 
