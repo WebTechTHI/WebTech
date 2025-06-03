984c986a8e2d24ae94f5e749f3edf42f4ec73a7b
micha <pietschi2003@gmail.com>
2025-06-03 18:27:41 +0200

    basic changes

---
+++ productLists/configurator.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MLR - Gaming PCs, Laptops & High-End Computer</title>
+
+    <link rel="stylesheet" href="/assets/css/index.css">
+    <link rel="stylesheet" href="/assets/css/mystyle.css">
+
+    <script src="/assets/javascript/toggleTheme.js"></script>
+    <script src="/assets/javascript/changeHero.js"></script>
+
+</head>
+<body>
+
+    <?php include 'components/header.html'; ?>
+
+    <?php include 'components/footer.html'; ?>
+    
+</body>
