21da08ac19a8a47fba0bae61ae2cc40723ea5a23
ris2830 <ris2830@thi.de>
2025-06-12 21:19:37 +0200

    a

--- category.php
+++ category.php
@@ -244,7 +244,7 @@ $categoryInfo = getCategoryInfo($category);
                             <div class="product-image">
                                 <a class="product-image-buy"
                                     href="/productPages/product.php?id=<?php echo $product['product_id']; ?>"> <img
-                                      src="/uploads/Screenshot 2025-05-21 000750.png" 
+                                      src="<?php echo htmlspecialchars($firstImage); ?>" 
                                         alt="<?php echo htmlspecialchars($product['alt_text'] ?? $product['name']); ?>"></a>
                             </div>
                             </a>
