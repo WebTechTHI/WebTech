d8b54ba3f36309ce1ee04faf89c76985206a474e
micha <pietschi2003@gmail.com>
2025-06-12 14:45:53 +0200

    a

--- index.php
+++ index.php
@@ -38,7 +38,7 @@
 
     <!-- Bestseller Section -->
     <section class="container">
-        <h2 class="section-title">BESTwwwwwwwwwwwwwwwwwwwwSELLER</h2>
+        <h2 class="section-title">BESTSELLER</h2>
 
 
 
