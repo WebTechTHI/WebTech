51391609979e9e0dea266ae167a2ec24f6c148ca
Laurinwinfo <lau-ps4@gmx.de>
2025-06-14 15:07:57 +0200

    a

--- pages/logout.php
+++ pages/logout.php
@@ -1,6 +1,5 @@
 <?php
   session_start();
-  session_unset();  //Inhalte löschen aus der session
-  session_destroy(); //session selbst löschen (session destroy lscht nicht inhalte von session deswegen unset wichtig)
-          //mit inhalte meine ich die variablen ===> dh. die variablen der session werden ncith geleert im skript sondern existieren noch weiter !!!!
+  session_unset(); 
+  session_destroy(); 
   ?>
