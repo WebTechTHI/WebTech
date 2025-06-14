ccb55d19234e2b46a30451397bd76c06b1740e9f
ris2830 <ris2830@thi.de>
2025-06-14 21:39:20 +0200

    a

--- model/CategoryModel.php
+++ model/CategoryModel.php
@@ -1,29 +1,29 @@
 <?php
 require_once 'db_verbindung.php';
+require_once 'categoryFunctions.php';
 
 class CategoryModel
 {
     public function getProducts($category, $orderBy, $direction)
     {
-        require_once 'categoryFunctions.php';
+        
         return getProductsByCategory($GLOBALS['conn'], $category, $orderBy, $direction);
     }
 
     public function getCategoryInfo($category)
     {
-        require_once 'categoryFunctions.php';
+        
         return getCategoryInfo($category);
     }
 
     public function getProductImages($productId)
     {
-        require_once 'categoryFunctions.php';
+        
         return getProductImages($GLOBALS['conn'], $productId);
     }
 
     public function buildSpecifications($product)
     {
-        require_once 'categoryFunctions.php';
         return buildSpecifications($product);
     }
 }
