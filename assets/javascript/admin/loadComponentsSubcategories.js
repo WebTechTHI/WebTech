8de6e94ea4395a43e372f1c3e14c5943ed3cdfe9
Mikel <pietschi2003@gmail.com>
2025-06-24 01:57:09 +0200

    admin css

---
+++ assets/javascript/admin/loadComponentsSubcategories.js
@@ -0,0 +1,23 @@
+
+//Abwarten bis dom geladen ist, sonst werden dieelemente nicht gefunden
+document.addEventListener("DOMContentLoaded", function () {
+
+//asynchrones Laden von unterkategorien und entsprechenden Komponenten bei änderung der Hauptkategorie 
+    document.getElementById("category").addEventListener("change", function () {
+      const category = this.value;
+
+      //Laden von unterkategorien in select-Auswahl
+      fetch("/view/admin/adminLoader/AdminSubcategoryLoader.php?category=" + category)
+        .then(response => response.text())
+        .then(data => {
+          document.getElementById("subcategory-container").innerHTML = data;
+        });
+
+      //dynamisches Laden von Komponenten zur Auswahl
+      fetch("/view/admin/adminLoader/AdminComponentLoader.php?category=" + category)
+        .then(response => response.text())
+        .then(data => {
+          document.getElementById("component-container").innerHTML = data;
+        });
+    });
+});
--- view/admin/adminPages/CreateProducts.php
+++ view/admin/adminPages/CreateProducts.php
@@ -7,6 +7,9 @@
   <title>Admin Bereich - Produktverwaltung</title>
   <link rel="icon" href="/assets/images/logo/favicon.png" type="image/x-icon" />
 
+  <!-- Script -->
+  <script src="/assets/javascript/admin/loadComponentsSubcategories.js"></script>
+
   <!-- Styling -->
   <link rel="stylesheet" href="/assets/css/colors.css" />
   <link rel="stylesheet" href="/assets/css/admin.css" />
@@ -15,93 +18,78 @@
 <body>
   <?php $category = $_POST["category"] ?? ""; ?>
 
-  <h1 style="text-align: center; color: var(--headline-color);">Produkte hochladen</h1>
+  <div class="page-wrapper">
+
+    <a class="back-last-page" href="/index.php?page=admin">Zurück</a>
+
+    <h1 style="text-align: center; color: var(--headline-color);">Produkte hochladen</h1>
 
 
-  <!-- Forumular zum anlegen neuer Produkte -->
-  <form class="admin-form" method="post" action="/index.php?page=admin&action=uploadSubmit"
-    enctype="multipart/form-data">
+    <!-- Forumular zum anlegen neuer Produkte -->
+    <form class="admin-form" method="post" action="/index.php?page=admin&action=uploadSubmit"
+      enctype="multipart/form-data">
 
-    <h2>Neues Produkt anlegen.</h2>
+      <h2>Neues Produkt anlegen.</h2>
 
 
-    <!-- Als erstes muss eine Kategorie ausgewählt werden, um anschließend die richtigen Untekategorien und Komponenten zu laden.-->
-    <label for="category">Bitte wählen Sie eine Produktkategorie:</label>
-    <select name="category" id="category">
-      <option value="" disabled <?= $category == "" ? "selected" : "" ?>>Bitte Kategorie wählen</option>
-      <option value="desktop" <?= $category == "desktop" ? "selected" : "" ?>>Desktop-PC</option>
-      <option value="laptop" <?= $category == "laptop" ? "selected" : "" ?>>Laptop</option>
-      <option value="accesories" <?= $category == "accesories" ? "selected" : "" ?>>Zubehör</option>
-    </select>
-
-    <!-- Unterkategorie wählen-->
-    <label for="subcategory">Bitte wählen Sie die passende Unterkategorie:</label>
-    <div id="subcategory-container">
-      <select name="subcategory" id="subcategory" disabled>
-        <option>Bitte zuerst eine Kategorie wählen</option>
+      <!-- Als erstes muss eine Kategorie ausgewählt werden, um anschließend die richtigen Untekategorien und Komponenten zu laden.-->
+      <label for="category">Bitte wählen Sie eine Produktkategorie:</label>
+      <select name="category" id="category">
+        <option value="" disabled <?= $category == "" ? "selected" : "" ?>>Bitte Kategorie wählen</option>
+        <option value="desktop" <?= $category == "desktop" ? "selected" : "" ?>>Desktop-PC</option>
+        <option value="laptop" <?= $category == "laptop" ? "selected" : "" ?>>Laptop</option>
+        <option value="accesories" <?= $category == "accesories" ? "selected" : "" ?>>Zubehör</option>
       </select>
-    </div>
 
-    <!-- Produktbild hochladen-->
-    <label for="image">Bild hochladen:</label>
-    <input type="file" name="images[]" id="image" multiple>
+      <!-- Unterkategorie wählen-->
+      <label for="subcategory">Bitte wählen Sie die passende Unterkategorie:</label>
+      <div id="subcategory-container">
+        <select name="subcategory" id="subcategory" disabled>
+          <option>Bitte zuerst eine Kategorie wählen</option>
+        </select>
+      </div>
 
-    <!-- Produktname eingeben-->
-    <label for="name">Name:</label>
-    <input type="text" name="name" id="name" >
+      <!-- Produktbild hochladen-->
+      <label for="image">Bild hochladen:</label>
+      <input type="file" name="images[]" id="image" multiple>
 
-    <!-- Produktbeschreibung eingeben-->
-    <label for="short-description">Kurzbeschreibung:</label>
-    <input type="text" name="short-description" id="short-description" >
+      <!-- Produktname eingeben-->
+      <label for="name">Name:</label>
+      <input type="text" name="name" id="name">
 
-    <!-- Produktbeschreibung eingeben-->
-    <label for="description">Beschreibung:</label>
-    <input type="text" name="description" id="description">
+      <!-- Produktbeschreibung eingeben-->
+      <label for="short-description">Kurzbeschreibung:</label>
+      <input type="text" name="short-description" id="short-description">
 
-
-    <!-- container für dynamisches befüllen der Komponentenauswahl -->
-    <div id="component-container"></div>
-
-
-    <!-- Preis in Euro-->
-    <label for="price">Preis:</label>
-    <input type="text" name="price" id="price">
+      <!-- Produktbeschreibung eingeben-->
+      <label for="description">Beschreibung:</label>
+      <input type="text" name="description" id="description">
 
 
-    <!-- Sale an oder aus -->
-    <label for="sale">Sale</label>
-    <select name="sale" id="sale">
-      <option value="0" selected>Standardpreis</option>
-      <option value="1">Im Angebot</option>
-    </select>
+      <!-- container für dynamisches befüllen der Komponentenauswahl -->
+      <div id="component-container"></div>
+
+
+      <!-- Preis in Euro-->
+      <label for="price">Preis:</label>
+      <input type="text" name="price" id="price">
+
+
+      <!-- Sale an oder aus -->
+      <label for="sale">Sale</label>
+      <select name="sale" id="sale">
+        <option value="0" selected>Standardpreis</option>
+        <option value="1">Im Angebot</option>
+      </select>
 
 
 
-    <input type="submit" value="Absenden" >
-  </form>
+      <input type="submit" value="Absenden">
+    </form>
 
-  <script>
-
-    //asynchrones Laden von unterkategorien und entsprechenden Komponenten bei änderung der Hauptkategorie 
-    document.getElementById("category").addEventListener("change", function () {
-      const category = this.value;
+  </div>
 
 
-      //Laden von unterkategorien in select-Auswahl
-      fetch("/view/admin/adminLoader/AdminSubcategoryLoader.php?category=" + category)
-        .then(response => response.text())
-        .then(data => {
-          document.getElementById("subcategory-container").innerHTML = data;
-        });
-
-      //dynamisches Laden von Komponenten zur Auswahl
-      fetch("/view/admin/adminLoader/AdminComponentLoader.php?category=" + category)
-        .then(response => response.text())
-        .then(data => {
-          document.getElementById("component-container").innerHTML = data;
-        });
-    });
-  </script>
 </body>
 
 </html>
