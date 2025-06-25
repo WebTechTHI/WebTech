0b3707e9491f13d902463e77b486d8b4703e951d
Laurinwinfo <Lau-ps4@gmx.de>
2025-06-25 09:28:10 +0200

    a

--- db_verbindung.php
+++ db_verbindung.php
@@ -8,6 +8,7 @@ $dbname = "onlineshop";
 //Verbindung herstellen
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 
+// Zeichensatz utf8mb4 sorgt dafür, dass Umlaute und Emojis richtig funktionieren.
 mysqli_set_charset($conn, "utf8mb4");
 
 //Verbindung prüfen oder fehlgeschlagen
