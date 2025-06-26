e09028cfb0a4f66e58a0222bb9311599a5457330
ris2830 <ris2830@thi.de>
2025-06-26 20:44:50 +0200

    bearbeitungsstatus in datenbank agepasst

--- model/OrderSuccessModel.php
+++ model/OrderSuccessModel.php
@@ -2,17 +2,22 @@
 // model/CheckoutModel.php
 
 require_once __DIR__ . '/../db_verbindung.php'; // Datenbankverbindung
-class CheckoutModel {
-    public function getOrderById($orderId, $userId) {
+class CheckoutModel
+{
+    public function getOrderById($orderId, $userId)
+    {
         global $conn;
 
         // 1. Hole die Kopfdaten der Bestellung
-        $sqlOrder = "SELECT * FROM orders WHERE order_id = ? AND user_id = ?";
+        $sqlOrder = "SELECT o.*, s.status_name 
+                    FROM orders o 
+                    JOIN order_status s ON o.status_id = s.order_status_id 
+                    WHERE o.order_id = ? AND o.user_id = ?";
         $stmtOrder = $conn->prepare($sqlOrder);
         $stmtOrder->bind_param('ii', $orderId, $userId);
         $stmtOrder->execute();
         $resultOrder = $stmtOrder->get_result();
-        
+
         $order = $resultOrder->fetch_assoc();
 
         if (!$order) {
