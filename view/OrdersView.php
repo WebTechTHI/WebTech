d5cb67a397ec0f88b9aef0d2324c4e570e6bbaad
ris2830 <ris2830@thi.de>
2025-06-27 01:35:37 +0200

    a

--- view/OrdersView.php
+++ view/OrdersView.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>MLR | Meine Bestellungen</title>
 
-    <link rel="stylesheet" href="/assets/css/ordersPage.css"> <!-- Du musst diese CSS-Datei noch erstellen -->
+    <link rel="stylesheet" href="/assets/css/ordersPage.css"> 
 </head>
 
 <body>
